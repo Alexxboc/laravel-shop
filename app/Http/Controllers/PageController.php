e63b390341e0344d14cf7b78e44bf451b248188a
FabioLaForesta <fabiolaforesta@icloud.com>
2022-06-15 13:05:23 +0200

    Create page controller #4

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        # code...
+    }
+    public function about()
+    {
+        # code...
+    }
+    public function contact()
+    {
+        # code...
+    }
+}
