2795ea450f7754351d280117dad3a458109b6770
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-06-15 12:56:04 +0200

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contacts', function () {
+    return view('contacts');
+})->name('contacts');
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
+
+//Create home, about, contacts, products routes.
